1563b2396b1ed09ac259274ea59d1ecf4db78b5d
pratik-athawale <atpratik5@gmail.com>
2025-10-02 17:49:26 +0530

    created express app with optimized image

--- optimizing-images/index.js
+++ optimizing-images/index.js
@@ -1,1 +1,12 @@
-console.log("Hello world");
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello from express app');
+})
+
+app.listen(port, () => {
+    console.log(`Express server listening on port ${port}`);
+})
